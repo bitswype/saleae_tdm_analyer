378b601ddda66b530c580276ad6cee95b7903f48
Chris Keeser <chriskeeser@oculus.com>
2022-08-02 22:11:56 -0700

    Add extended PCM wave file header option

--- src/TdmAnalyzerResults.h
+++ src/TdmAnalyzerResults.h
@@ -64,6 +64,35 @@ typedef struct
 #pragma pack(pop)
 #pragma scalar_storage_order default
 
+#pragma scalar_storage_order little-endian
+#pragma pack(push, 1)
+typedef struct
+{
+    char mRiffCkId[4] = {'R', 'I', 'F', 'F'};   // @ 0,
+    U32 mRiffCkSize = 72;                       // @ 4,
+    char mRiffWaveId[4] = {'W', 'A', 'V', 'E'}; // @ 8, 
+    char mFmtCkId[4] = {'f', 'm', 't', ' '};    // @ 12,
+    U32 mFmtCkSize = 40;                        // @ 16, 
+    U16 mFormatTag = 0xFFFE;                    // @ 20,
+    U16 mNumChannels = 1;                       // @ 22,
+    U32 mSamplesPerSec = 8000;                  // @ 24, 
+    U32 mBytesPerSec = 16000;                   // @ 28,
+    U16 mBlockSizeBytes = 2;                    // @ 32,
+    U16 mBitsPerSample = 16;                    // @ 34,
+    U16 mExtensionSizeBytes = 22;               // @ 36,
+    U16 mDataBitsPerSample = 16;                // @ 38,
+    U32 mChannelMask = 0;                       // @ 40,
+    U8 mSubFormatGUID[16] = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x80, 0x00, 0x00, 0xAA, 0x00, 0x38, 0x9B, 0x71}; // @ 44,
+    char mFactCkId[4] = {'f', 'a', 'c', 't'};   // @ 60,
+    U32 mFactCkSize = 4;                        // @ 64,
+    U32 mSampleLength = 0;                      // @ 68,
+    char mDataCkId[4] = {'d' , 'a', 't', 'a'};  // @ 72,
+    U32 mDataCkSize = 0;                        // @ 76,
+    /* data */                                  // @ 80
+} WavePCMExtendedHeader;
+#pragma pack(pop)
+#pragma scalar_storage_order default
+
 class PCMWaveFileHandler
 {
   public:
@@ -92,6 +121,37 @@ class PCMWaveFileHandler
 
     constexpr static U64 RIFF_CKSIZE_POS = 4;
     constexpr static U64 DATA_CKSIZE_POS = 40;
+};
+
+class PCMExtendedWaveFileHandler
+{
+  public:
+    PCMExtendedWaveFileHandler(std::ofstream & file, U32 sample_rate = 48000, U32 num_channels = 2, U32 bits_per_channel = 32);
+    ~PCMExtendedWaveFileHandler();
+
+    void addSample(U64 sample);
+    void close(void);
+
+  private:
+    void writeLittleEndianData(U64 value, U8 num_bytes);
+    void updateFileSize();
+
+  private:
+    WavePCMExtendedHeader mWaveHeader;
+    U32 mNumChannels;
+    U32 mBitsPerChannel;
+    U8 mBitShift;
+    U32 mBytesPerChannel;
+    U32 mSampleRate;
+    U32 mFrameSizeBytes;
+    U32 mSampleCount;
+    U32 mTotalFrames;
+    std::ofstream & mFile;
+    std::streampos mWtPosSaved;
+
+    constexpr static U64 EXT_RIFF_CKSIZE_POS = 4;
+    constexpr static U64 EXT_FACT_CKSIZE_POS = 68;
+    constexpr static U64 EXT_DATA_CKSIZE_POS = 76;
 };
 
 #endif // TDM_ANALYZER_RESULTS
